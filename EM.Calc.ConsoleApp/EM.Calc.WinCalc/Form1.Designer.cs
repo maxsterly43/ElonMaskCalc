d8b4d10e242b966cfcfd6c4f3f82072c83fabe61
Elsukov Maxim <elsukov43@gmail.com>
2018-10-31 15:39:21 +0300

    create WinCalc

---
+++ EM.Calc.ConsoleApp/EM.Calc.WinCalc/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace EM.Calc.WinCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbOperation = new System.Windows.Forms.ComboBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.input = new System.Windows.Forms.TextBox();
+            this.btnExec = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbOperation
+            // 
+            this.cbOperation.FormattingEnabled = true;
+            this.cbOperation.Location = new System.Drawing.Point(12, 12);
+            this.cbOperation.Name = "cbOperation";
+            this.cbOperation.Size = new System.Drawing.Size(121, 21);
+            this.cbOperation.TabIndex = 0;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(9, 132);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(59, 13);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Text = "Результат";
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(12, 78);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(100, 20);
+            this.input.TabIndex = 2;
+            // 
+            // btnExec
+            // 
+            this.btnExec.Location = new System.Drawing.Point(88, 127);
+            this.btnExec.Name = "btnExec";
+            this.btnExec.Size = new System.Drawing.Size(75, 23);
+            this.btnExec.TabIndex = 3;
+            this.btnExec.Text = "Вычислить";
+            this.btnExec.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExec);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.cbOperation);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbOperation;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button btnExec;
+    }
+}
+
